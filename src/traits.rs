401707c2745fe2994b6a07068354cebf81ba097f
cozyGalvinism <reallifejunkies@googlemail.com>
2021-09-08 19:46:13 +0200

    Add new trait called YouTubeSendable, this is experimental so far

--- src/traits.rs
+++ src/traits.rs
@@ -8,12 +8,17 @@ use crate::structs::Message;
 ///
 /// This trait is an async_trait, which means that you can use async/await syntax.
 #[async_trait]
-pub trait Command: Send + Sync + DynClone {
-    async fn execute(&self, message: Message) -> Result<(), CommandError>;
+pub trait Command<YT>: Send + Sync + DynClone where YT: YouTubeSendable {
+    async fn execute(&self, message: Message, sendable: &YT) -> Result<(), CommandError>;
 }
-dyn_clone::clone_trait_object!(Command);
+dyn_clone::clone_trait_object!(Command<dyn YouTubeSendable>);
 
 /// Types that implement this trait register commands.
 pub trait CommandRegistrar {
-    fn register_command(&mut self, name: &str, aliases: &[&str], command: Box<dyn Command>);
+    fn register_command(&mut self, name: &str, aliases: &[&str], command: Box<dyn Command<dyn YouTubeSendable>>);
+    fn send_message(&self, message: &str);
+}
+
+pub trait YouTubeSendable {
+    fn send_message(&self, message: &str);
 }
